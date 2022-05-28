1d6d6953b693e5fae148334b93babdaf45380466
Jack <jhlee409@naver.com>
2022-05-28 16:57:50 +0900

    [SETTING] setup next config => routing page rule

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+export const nextConfig = {
+  compress: true,
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+  pageExtensions: ["page.tsx"],
+};
+
+const config = nextConfig;
+module.exports = config;
